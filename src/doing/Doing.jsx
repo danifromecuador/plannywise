d9dd14321f1b91f0e32be527783dd8f230e43f8f
dani <danifromecuador@gmail.com>
2024-08-07 20:02:01 -0500

    Refactor clock countdown to adapt to the App style branch feat/doing

--- src/doing/Doing.jsx
+++ src/doing/Doing.jsx
@@ -1,29 +1,61 @@
-import React, { useState, useRef } from 'react'
-import Countdown from 'react-countdown'
+import { useState, useRef } from 'react'
+import Countdown, { zeroPad } from 'react-countdown'
+import './Doing.css'
 
 export const Doing = () => {
-  const [date, setDate] = useState(Date.now() + 61000)
+  const [textStartBtn, setTextStartBtn] = useState("START")
+  const [viewStartBtn, setViewStartBtn] = useState("")
+  const [viewPauseBtn, setViewPauseBtn] = useState("hide")
+  const [viewResetBtn, setViewResetBtn] = useState("hide")
+  const [date, setDate] = useState(Date.now() + 900000)
   const countdownRef = useRef(null)
 
-  const handleStartClick = () => { if (countdownRef.current) countdownRef.current.getApi().start() }
-  const handlePauseClick = () => { if (countdownRef.current) countdownRef.current.getApi().pause() }
-  const handleResetClick = () => setDate(Date.now() + 61000)
+  const handleStartClick = () => {
+    if (countdownRef.current) countdownRef.current.getApi().start()
+    setViewStartBtn("hide")
+    setViewPauseBtn("")
+    setViewResetBtn("")
+  }
+
+  const handlePauseClick = () => {
+    if (countdownRef.current) countdownRef.current.getApi().pause()
+    setTextStartBtn("CONTINUE")
+    setViewStartBtn("")
+    setViewPauseBtn("hide")
+  }
+
+  const handleResetClick = () => {
+    setDate(Date.now() + 900000)
+    setTextStartBtn("START")
+    setViewStartBtn("")
+    setViewPauseBtn("hide")
+    setViewResetBtn("hide")
+  }
+
+  const rendered = ({ minutes, seconds }) => <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>
 
   return (
-    <>
-      <h3>Countdown with Start, Pause and Reset Controls</h3>
-      <Countdown
-        key={date}
-        ref={countdownRef}
-        date={date}
-        autoStart={false}
-      />
-      <div>
-        <button type="button" onClick={handleStartClick}>Start</button>
-        <button type="button" onClick={handlePauseClick}>Pause</button>
-        <button type="button" onClick={handleResetClick}>Reset</button>
+    <div className='Doing'>
+      <h1>Doing</h1>
+      <div className="pomodoro">
+        <div className="clock">
+          <Countdown
+            ref={countdownRef}
+            date={date}
+            autoStart={false}
+            renderer={rendered}
+          />
+        </div>
+        <div className="controls">
+          <button className={`${viewStartBtn} start`} onClick={handleStartClick}>{textStartBtn}</button>
+          <button className={`${viewPauseBtn} pause`} onClick={handlePauseClick}>PAUSE</button>
+          <button className={`${viewResetBtn} reset`} onClick={handleResetClick}>RESET</button>
+        </div>
+        <div className="motivational">
+          <p>You only fall when you stop trying!</p>
+        </div>
       </div>
-    </>
+    </div >
   )
 }
 
