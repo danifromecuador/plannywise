c7a786f5d79f431635ef6858e0f86d1d3d57a7a1
dani <danifromecuador@gmail.com>
2024-08-12 13:05:51 -0500

    Add minimal style to Todo & Todos components branch feat/todo

--- src/todo/Todos.jsx
+++ src/todo/Todos.jsx
@@ -1,8 +1,10 @@
+import './Todos.css'
+
 export const Todos = ({ title, completedText, deleteBtnText, inputText }) => {
   return (
     <div className='Todos'>
       <header>
-        <span>{completedText? completedText : "Completed"}:<span>16</span>{completedText? "h" : "%"}</span>
+        <span>{completedText? completedText : "Completed"}: <span>16{completedText? "h" : "%"}</span></span>
         <h1>{title}</h1>
       </header>
       <ul>
@@ -14,7 +16,6 @@ export const Todos = ({ title, completedText, deleteBtnText, inputText }) => {
         <button>{deleteBtnText? deleteBtnText : "Delete Completed Goals"}</button>
         <input type="text" defaultValue={inputText? inputText : "Type a new goal and press Enter"} />
       </footer>
-
     </div>
   )
 }
