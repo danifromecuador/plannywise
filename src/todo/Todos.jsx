614e53c92624b1c3bf2dd4f762a44b5476e254c6
dani <danifromecuador@gmail.com>
2024-08-12 12:45:09 -0500

    Create Todos component to be reused 4 times branch feat/todo

---
+++ src/todo/Todos.jsx
@@ -0,0 +1,20 @@
+export const Todos = ({ title, completedText, deleteBtnText, inputText }) => {
+  return (
+    <div className='Todos'>
+      <header>
+        <span>{completedText? completedText : "Completed"}:<span>16</span>{completedText? "h" : "%"}</span>
+        <h1>{title}</h1>
+      </header>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      <footer>
+        <button>{deleteBtnText? deleteBtnText : "Delete Completed Goals"}</button>
+        <input type="text" defaultValue={inputText? inputText : "Type a new goal and press Enter"} />
+      </footer>
+
+    </div>
+  )
+}
