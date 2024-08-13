1c44eae5b33d1646838849ae84d9d0e13d22cbe7
dani <danifromecuador@gmail.com>
2024-08-12 19:30:41 -0500

    Remove commented code branch feat/todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,8 @@ import {Done} from './done/Done.jsx'
 import './App.css'
 
 export const App = () => {
-  // const store = Store()
   return (
     <div className='App'>
-      {/* <h1>App Component</h1>
-      <div>Bears: {store.bears.amount}</div>
-      <div>Cows: {store.cows.amount}</div>
-      <button onClick={()=>store.bears.add()}>Add bear</button> */}
       <Todos />
       <Doing />
       <Done />
