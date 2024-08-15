c77ca0793df7a3645b061de8f135f96f910babfb
dani <danifromecuador@gmail.com>
2024-08-14 23:27:29 -0500

    Give an aditional prop to each function to avoid creating add and other functions for each daily weekly and monthly branch feat/todo

--- src/store/todo_logic.js
+++ src/store/todo_logic.js
@@ -1,33 +1,33 @@
-export const add = (set, input) => set((state) => ({
-  daily: {
-    ...state.daily,
-    todos: [...state.daily.todos, { id: Date.now(), content: input }]
+export const add = (set, input, sliceID) => set((state) => ({
+  [sliceID]: {
+    ...state[sliceID],
+    todos: [...state[sliceID].todos, { id: Date.now(), content: input }]
   }
 }))
 
-export const markAsDone = (set, item) => set((state) => ({
-  daily: {
-    ...state.daily,
-    todos: state.daily.todos.filter(i => i.id !== item.id),
-    dones: [...state.daily.dones, item].sort((a, b) => a.id - b.id)
+export const markAsDone = (set, item, sliceID) => set((state) => ({
+  [sliceID]: {
+    ...state[sliceID],
+    todos: state[sliceID].todos.filter(i => i.id !== item.id),
+    dones: [...state[sliceID].dones, item].sort((a, b) => a.id - b.id)
   }
 }))
 
-export const markAsTodo = (set, item) => set((state) => ({
-  daily: {
-    ...state.daily,
-    todos: [...state.daily.todos, item].sort((a, b) => a.id - b.id),
-    dones: state.daily.dones.filter(i => i.id !== item.id)
+export const markAsTodo = (set, item, sliceID) => set((state) => ({
+  [sliceID]: {
+    ...state[sliceID],
+    todos: [...state[sliceID].todos, item].sort((a, b) => a.id - b.id),
+    dones: state[sliceID].dones.filter(i => i.id !== item.id)
   }
 }))
 
-export const completed = (get) => (
-  `${(Math.floor((get().daily.dones.length / (get().daily.todos.length + get().daily.dones.length))*100)).toString()}%`
+export const completed = (get, sliceID) => (
+  `${(Math.floor((get()[sliceID].dones.length / (get()[sliceID].todos.length + get()[sliceID].dones.length))*100)).toString()}%`
 )
 
-export const deleteDones = (set) => set((state)=>({
-  daily: {
-    ...state.daily,
+export const deleteDones = (set, sliceID) => set((state)=>({
+  [sliceID]: {
+    ...state[sliceID],
     dones: []
   }
 }))
