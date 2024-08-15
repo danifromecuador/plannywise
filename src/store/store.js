8b8bcecc21da6d70b71c0f0ed9b6a89f685a44f1
dani <danifromecuador@gmail.com>
2024-08-14 23:26:40 -0500

    Create slices for weekly and monthly data on Store branch feat/todo

--- src/store/store.js
+++ src/store/store.js
@@ -4,15 +4,39 @@ import { add, completed, markAsDone, markAsTodo, deleteDones } from "./todo_logi
 
 const todoDailySlice = (set, get) => ({
   title: "Daily Goals",
-  completed: () => completed(get),
-  todos: JSON.parse(localStorage.getItem("dailyTodos")) || [],
-  dones: JSON.parse(localStorage.getItem("dailyDones")) || [],
-  add: (input) => add(set, input),
-  markAsDone: (item) => markAsDone(set, item),
-  markAsTodo: (item) => markAsTodo(set, item),
-  deleteDones: () => deleteDones(set)
+  completed: (sliceID) => completed(get, sliceID),
+  todos: JSON.parse(localStorage.getItem("Daily Goals Todos")) || [],
+  dones: JSON.parse(localStorage.getItem("Daily Goals Dones")) || [],
+  add: (input, sliceID) => add(set, input, sliceID),
+  markAsDone: (item, sliceID) => markAsDone(set, item, sliceID),
+  markAsTodo: (item, sliceID) => markAsTodo(set, item, sliceID),
+  deleteDones: (sliceID) => deleteDones(set, sliceID)
+})
+
+const todoWeeklySlice = (set, get) => ({
+  title: "Weekly Goals",
+  completed: (sliceID) => completed(get, sliceID),
+  todos: JSON.parse(localStorage.getItem("Weekly Goals Todos")) || [],
+  dones: JSON.parse(localStorage.getItem("Weekly Goals Todos")) || [],
+  add: (input, sliceID) => add(set, input, sliceID),
+  markAsDone: (item, sliceID) => markAsDone(set, item, sliceID),
+  markAsTodo: (item, sliceID) => markAsTodo(set, item, sliceID),
+  deleteDones: (sliceID) => deleteDones(set, sliceID)
+})
+
+const todoMonthlySlice = (set, get) => ({
+  title: "Monthly Goals",
+  completed: (sliceID) => completed(get, sliceID),
+  todos: JSON.parse(localStorage.getItem("Monthly Goals Todos")) || [],
+  dones: JSON.parse(localStorage.getItem("Monthly Goals Todos")) || [],
+  add: (input, sliceID) => add(set, input, sliceID),
+  markAsDone: (item, sliceID) => markAsDone(set, item, sliceID),
+  markAsTodo: (item, sliceID) => markAsTodo(set, item, sliceID),
+  deleteDones: (sliceID) => deleteDones(set, sliceID)
 })
 
 export const Store = create(devtools((set, get) => ({
-  daily: todoDailySlice(set, get)
+  daily: todoDailySlice(set, get),
+  weekly: todoWeeklySlice(set, get),
+  monthly: todoMonthlySlice(set, get)
 })))
