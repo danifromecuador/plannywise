9c0aecb1ca5597db170aa93837ace5a56753f26c
dani <danifromecuador@gmail.com>
2024-08-19 21:08:55 -0500

    Create layout of Done.jsx component branch feat/done

--- src/done/Done.jsx
+++ src/done/Done.jsx
@@ -1,9 +1,30 @@
 import './Done.css'
-
+import { Store } from '../store/store'
 export const Done = () => {
+  const store = Store()
   return (
     <div className='Done'>
       <h1>Done</h1>
+      <div className="worked-hours">
+        <h2>Worked Hours</h2>
+        <div className="this-month">This Month: 23</div>
+        <div className="this-week">This Week: 23</div>
+        <div className="this-day">This Day: 23</div>
+      </div>
+      <div className="completed-tasks-container">
+        <ul>
+          <li>reading</li>
+          <li>reading</li>
+          <li>reading</li>
+        </ul>
+        <footer>
+          <button>Delete All Completed</button>
+        </footer>
+        <input
+         type="text"
+          placeholder='Type a completed task and press Enter'
+         />
+      </div>
     </div>
   )
 }
