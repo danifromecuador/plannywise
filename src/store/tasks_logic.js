065a0487a532397fbd1f9a131b99ad3fc8b8510c
dani <danifromecuador@gmail.com>
2024-08-27 14:37:13 -0500

    Change array type from stack to queue for deleteAllCompletedTasks branch feat/done

--- src/store/tasks_logic.js
+++ src/store/tasks_logic.js
@@ -5,17 +5,21 @@ export const addCompletedTask = (set, input) => set(state => ({
   }
 }), false, 'tasks/add')
 
-export const deleteAllCompletedTasks = (set, get) => set(state => ({
-  tasks: {
-    ...state.tasks,
-    workedHoursHistory: [...state.tasks.workedHoursHistory, get().tasks.completed.length * 15 / 60],
-    completed: []
-  }
-}), false, 'tasks/deleteAllCompleted')
+export const deleteAllCompletedTasks = (set, get) => set(state => {
+  const workedHoursHistoryArray = [get().tasks.completed.length * 15 / 60, ...state.tasks.workedHoursHistory]
+  workedHoursHistoryArray.length > 30 && workedHoursHistoryArray.pop()
+  return ({
+    tasks: {
+      ...state.tasks,
+      workedHoursHistory: workedHoursHistoryArray,
+      completed: []
+    }
+  })
+}, false, 'tasks/deleteAllCompleted')
 
 export const workedHours = get => {
   const day = get().tasks.completed.length * 15 / 60
-  const week = get().tasks.workedHoursHistory.slice(-7).reduce((a, b)=> a+b, 0)
-  const month = get().tasks.workedHoursHistory.slice(-30).reduce((a, b)=> a+b, 0)
+  const week = get().tasks.workedHoursHistory.slice(0, 7).reduce((a, b) => a + b, 0)
+  const month = get().tasks.workedHoursHistory.reduce((a, b) => a + b, 0)
   return ({ "day": day, "week": week, "month": month })
 }
