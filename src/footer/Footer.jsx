04777b6c23aef6ba93f87244024ce6dacdaf99fa
dani <danifromecuador@gmail.com>
2024-08-29 13:01:36 -0500

    Avoid being too verbose on Footer messages branch feat/footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <div className={`${showFooter} Footer`}>
       <span>Created by  <a href="https://www.linkedin.com/in/danifromec/" target="_blank" rel="noopener noreferrer"> Dani Dev</a></span>
       <span>If you enjoy this app, please <a href="https://github.com/danifromecuador/plannywise" target='blank'>give it a ⭐</a></span>
-      <span>If you're not hearing the alarm sound, please check out <a href="https://github.com/danifromecuador/plannywise/issues/6" target='blank'>this issue</a></span>
+      <span>If you're not hearing the alarm sound, check out <a href="https://github.com/danifromecuador/plannywise/issues/6" target='blank'>this issue</a></span>
       <span>If you'd like to provide feedback, report issues, or suggest improvements, <a href="https://github.com/danifromecuador/plannywise/issues" target='blank'>click here</a></span>
       <button onClick={()=> setShowFooter("hide")}>X</button>
     </div>
