7ad6f9406463ba26a0dba4e1f3143a650bfe271b
dani <danifromecuador@gmail.com>
2024-08-29 12:26:59 -0500

    Add useful links to Footer component branch feat/footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -3,7 +3,11 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <div className='Footer'>
-      Footer Component
+      <span>Created by Dani Dev</span>
+      <span>If you enjoy this app, please <a href="http://" target='blank'>give it a ⭐</a></span>
+      <span>If you're not hearing the alarm sound, please check out <a href="http://" target='blank'>this issue</a></span>
+      <span>If you'd like to provide feedback, report issues, or suggest improvements, <a href="http://" target='blank'>click here</a></span>
+      <button>X</button>
     </div>
   )
 }
