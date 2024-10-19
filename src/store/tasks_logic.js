7a7cb035da99d4405022a7a30ce6331324b69655
dani <danifromecuador@gmail.com>
2024-10-18 21:40:06 -0500

    Include common tasks counter in the today global counter branch feat/common-completed-tasks

--- src/store/tasks_logic.js
+++ src/store/tasks_logic.js
@@ -6,20 +6,24 @@ export const addCompletedTask = (set, input) => set(state => ({
 }), false, 'tasks/add')
 
 export const deleteAllCompletedTasks = (set, get) => set(state => {
-  const workedHoursHistoryArray = [get().tasks.completed.length * 15 / 60, ...state.tasks.workedHoursHistory]
+  const commonTasksCounter = get().tasks.commonTasks.learn + get().tasks.commonTasks.code + get().tasks.commonTasks.apply
+  const workedHoursHistoryArray = [(get().tasks.completed.length * 15 / 60) + commonTasksCounter, ...state.tasks.workedHoursHistory]
   workedHoursHistoryArray.length > 30 && workedHoursHistoryArray.pop()
+  console.log(workedHoursHistoryArray)
+  console.log(workedHours(get))
   return ({
     tasks: {
       ...state.tasks,
       completed: [],
       workedHoursHistory: workedHoursHistoryArray,
-      commonTasks: {"learn": 0, "code": 0, "apply":0}
+      commonTasks: { "learn": 0, "code": 0, "apply": 0 }
     }
   })
 }, false, 'tasks/deleteAllCompleted')
 
 export const workedHours = get => {
-  const day = (get().tasks.completed.length * 15 / 60)+ get().tasks.commonTasks.learn + get().tasks.commonTasks.code + get().tasks.commonTasks.apply
+  const commonTasksCounter = get().tasks.commonTasks.learn + get().tasks.commonTasks.code + get().tasks.commonTasks.apply
+  const day = (get().tasks.completed.length * 15 / 60) + commonTasksCounter
   const week = get().tasks.workedHoursHistory.slice(0, 7).reduce((a, b) => a + b, 0)
   const month = get().tasks.workedHoursHistory.reduce((a, b) => a + b, 0)
   return ({ "day": day, "week": week, "month": month })
@@ -27,7 +31,7 @@ export const workedHours = get => {
 
 export const addCommonTask = (get, set, task) => set(state => {
   let commonTasks = { "learn": get().tasks.commonTasks.learn, "code": get().tasks.commonTasks.code, "apply": get().tasks.commonTasks.apply }
-  task === "learn" ? commonTasks.learn+=0.25 : task === "code" ? commonTasks.code+=0.25 : commonTasks.apply+=0.25
+  task === "learn" ? commonTasks.learn += 0.25 : task === "code" ? commonTasks.code += 0.25 : commonTasks.apply += 0.25
   return ({
     tasks: {
       ...state.tasks,
