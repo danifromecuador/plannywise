f6882bbf2b934b1846af8dbb7cf8adf5506e77a4
dani <danifromecuador@gmail.com>
2024-11-21 14:41:48 -0500

    Translate all Todos component feat/change-language

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './Todo.css'
 
-export const Todo = ({ store }) => {
-
+export const Todo = ({ store, text, title }) => {
   const [input, setInput] = useState("")
   const handleInputEnterKey = k => k.key === "Enter" && input.trim() != "" && (store.add(input), setInput(""))
 
@@ -15,20 +14,20 @@ export const Todo = ({ store }) => {
   return (
     <div className='Todo sub-container'>
       <header>
-        <div><span className={store.completed() === "NaN%" ? 'hide' : ''}>Completed: <span className='counter-stats'>{store.completed()}</span></span></div>
-        <h1>{store.title}</h1>
+        <div><span className={store.completed() === "NaN%" ? 'hide' : ''}>{text.todo.completed}: <span className='counter-stats'>{store.completed()}</span></span></div>
+        <h1>{title}</h1>
       </header>
       <ul className='ul'>
         {store.todos.map(item => (<li key={item.id} className='li' onClick={() => store.markAsDone(item)}>{item.content}</li>))}
         {store.dones.map(item => (<li key={item.id} className='li dones' onClick={() => store.markAsTodo(item)}>{item.content}</li>))}
       </ul>
       <footer>
-        <button className={`${store.dones.length === 0 ? 'hide' : 'midBtn'}`} onClick={() => store.deleteDones()} >Delete All Completed</button>
+        <button className={`${store.dones.length === 0 ? 'hide' : 'midBtn'}`} onClick={() => store.deleteDones()} >{text.todo.deleteAll}</button>
         <input
           type="text"
           className='input'
           value={input}
-          placeholder="Type a goal and press Enter"
+          placeholder={text.todo.placeHolder}
           onChange={e => setInput(e.target.value)}
           onKeyDown={k => handleInputEnterKey(k)}
         />
--- src/todo/Todos.jsx
+++ src/todo/Todos.jsx
@@ -3,13 +3,16 @@ import { Todo } from './Todo.jsx'
 
 export const Todos = () => {
   const store = Store()
+  const title = store.language.current === "english"
+    ? ["Daily Goals", "Weekly Goals", "Monthly Goals"]
+    : ["Objetivos Diarios", "Objetivos Semanales", "Objetivos Mensuales"]
   const text = store.language.current === "english" ? store.language.text().english : store.language.text().spanish
   return (
     <div className='Todos'>
       <h1>{text.todo.title}</h1>
-      <Todo store={store.daily} />
-      <Todo store={store.weekly} />
-      <Todo store={store.monthly} />
+      <Todo store={store.daily} text={text} title={title[0]} />
+      <Todo store={store.weekly} text={text} title={title[1]} />
+      <Todo store={store.monthly} text={text} title={title[2]} />
     </div>
   )
 }
