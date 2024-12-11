ef125b047662b201b4ac7e0eaa784736e15ca57d
dani <danifromecuador@gmail.com>
2024-12-11 12:55:05 -0500

    Separate Clock and Quote in components inside Doing component fix/doing-structure

---
+++ src/doing/Clock.jsx
@@ -0,0 +1,70 @@
+import './Clock.css'
+import Countdown, { zeroPad } from 'react-countdown'
+import { useState, useRef } from 'react'
+import { Store } from '../store/store.js'
+
+export const Clock = () => {
+  const store = Store()
+  const text = store.language.current === "english" ? store.language.text().english : store.language.text().spanish
+  const countdownRef = useRef(null)
+  const audioStart = new Audio('/start.mp3')
+  const audioAlarm = new Audio('/clock_alarm.mp3')
+  const [textStartBtn, setTextStartBtn] = useState(text.doing.pomodoro.start)
+  const [viewStartBtn, setViewStartBtn] = useState("")
+  const [viewPauseBtn, setViewPauseBtn] = useState("hide")
+  const [viewResetBtn, setViewResetBtn] = useState("hide")
+  const [date] = useState(Date.now() + 900000)
+
+  const rendered = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      audioAlarm.play()
+      handleResetClick(false)
+    }
+    return <span>{zeroPad(minutes)}:{zeroPad(seconds)}</span>
+  }
+
+  const handleStartClick = () => {
+    if (countdownRef.current) countdownRef.current.getApi().start()
+    setViewStartBtn("hide")
+    setViewPauseBtn("")
+    setViewResetBtn("")
+    audioStart.play()
+  }
+
+  const handlePauseClick = () => {
+    if (countdownRef.current) countdownRef.current.getApi().pause()
+    setTextStartBtn(text.doing.pomodoro.continue)
+    setViewStartBtn("")
+    setViewPauseBtn("hide")
+    audioStart.play()
+  }
+
+  const handleResetClick = (withSound) => {
+    if (countdownRef.current) countdownRef.current.getApi().stop()
+    setTextStartBtn(text.doing.pomodoro.start)
+    setViewStartBtn("")
+    setViewPauseBtn("hide")
+    setViewResetBtn("hide")
+    if (withSound) audioStart.play()
+  }
+
+  return (
+    <div className='Clock'>
+      <div className="pomodoro sub-container">
+        <div className="clock">
+          <Countdown
+            ref={countdownRef}
+            date={date}
+            autoStart={false}
+            renderer={rendered}
+          />
+        </div>
+        <div className="controls">
+          <button className={`${viewStartBtn} start bigBtn`} onClick={handleStartClick}>{textStartBtn}</button>
+          <button className={`${viewPauseBtn} pause bigBtn`} onClick={handlePauseClick}>{text.doing.pomodoro.pause}</button>
+          <button className={`${viewResetBtn} reset bigBtn`} onClick={() => handleResetClick(true)}>{text.doing.pomodoro.reset}</button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/doing/Quote.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Store } from '../store/store.js'
+import axios from 'axios'
+
+
+import './Quote.css'
+
+export const Quote = () => {
+  const store = Store()
+  const text = store.language.current === "english" ? store.language.text().english : store.language.text().spanish
+  const [quote, setQuote] = useState("")
+  const [author, setAuthor] = useState("")
+  const [fetchNewQuote, setFetchNewQoute] = useState("")
+  const catchNewQuote = () => fetchNewQuote == "yes" ? setFetchNewQoute("") : setFetchNewQoute("yes")
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await axios('https://api.quotable.io/random', {
+          params: {
+            tags: 'motivational|success|change|character|future|inspirational',
+            maxLength: 70,
+          }
+        })
+        setQuote(response.data.content.slice(0, -1)) // Delete the last dot of the quote
+        setAuthor(response.data.author)
+      }
+      catch (error) { console.error("Error fetching the quote: ", error.message) }
+    }
+    fetchQuote()
+  }, [fetchNewQuote])
+
+  const translateQuote = async () => {
+    let quoteTranslated = await axios(`https://api.mymemory.translated.net/get?q=${quote}&langpair=en|es`)
+    setQuote(quoteTranslated.data.responseData.translatedText)
+  }
+
+  useEffect(() => {
+    if (store.language.current === "spanish") translateQuote()
+  }, [author])
+  return (
+    <div className="motivational" onClick={catchNewQuote}>
+      <p>{quote}</p>
+      <p>{author}</p>
+    </div>
+  )
+}
