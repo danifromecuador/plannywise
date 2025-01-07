d6f4730b3e4273d9a366ff45f6ab77d13eb0f544
dani <danifromecuador@gmail.com>
2025-01-06 20:38:46 -0500

    Remove develop environment code (logs) feat/edit-common-tasks

--- src/store/common_tasks_logic.js
+++ src/store/common_tasks_logic.js
@@ -1,14 +1,9 @@
-export const addCT = (set, input) => set(state=>{
-  console.log(input);
-  
-  return ({
-    configs: {
-      ...state.configs,
-      commonTasks: {
-        ...state.configs.commonTasks,
-        currents: [...state.configs.commonTasks.currents, input]
-      }
+export const addCT = (set, input) => set(state => ({
+  configs: {
+    ...state.configs,
+    commonTasks: {
+      ...state.configs.commonTasks,
+      currents: [...state.configs.commonTasks.currents, input]
     }
-  })
-
-})
+  }
+}))
